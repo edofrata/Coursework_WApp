3a822fdb6ad08e142f2cd246245832fabf1109ff
Edoardo Fratantonio <ef364@mdx.live.ac.uk>
2021-10-23 15:51:43 +0100

    Added css file for styling the Web App

---
+++ insertion.js
@@ -0,0 +1,14 @@
+
+ let math_lesson = new Vue({
+                el: '#app',
+                data: {
+                    sitename: 'After School Club',
+                    product: {
+                        id: 3377,
+                        title: 'Math',
+                        location: 'London',
+                        price: 100,
+                        image: "math.png"
+                    }
+                }
+            });
