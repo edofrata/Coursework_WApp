418600f1cca296f0a3d48e037af1570777df9865
Edoardo Fratantonio <ef364@mdx.live.ac.uk>
2021-10-31 17:09:28 +0000

    Organised the project in folders and updated the path

---
+++ vue/lessons.js
@@ -0,0 +1,15 @@
+let vueapp = new Vue({
+    el: '#app',
+    data: {
+        siteimage: 'images/After School Lessons Club-logos_transparent.png',
+        product: {
+            id: 3377,
+            title: 'Math',
+            location: 'London',
+            price: 100,
+            image: "images/maths.png",
+            btn_shop: "Add to Cart"
+
+        }
+    }
+});
