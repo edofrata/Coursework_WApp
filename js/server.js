c248b94e1b1585b94090df80919f404466912dc2
Edoardo Fratantonio <ef364@mdx.live.ac.uk>
2022-01-20 22:58:42 +0000

    adjusted path in package.json

--- js/server.js
+++ js/server.js
@@ -99,8 +99,9 @@ app.use(function (request, response) {
     response.status(404).send("This page has not been made yet!");
 });
 
-app.listen(3000, function () {
-    console.log("Server started on port 3000");
+const port = process.env.PORT || 3000;
+app.listen(port, function () {
+    console.log("Server started on port " + port);
 });
 
 
